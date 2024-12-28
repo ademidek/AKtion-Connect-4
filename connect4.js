bcc391d8490e74f4ba125ba8c03d439e4c7bd42a
ademidek <ademidek@gmail.com>
2024-12-28 18:13:25 -0500

    added functionality for placing pieces on the board

--- connect4.js
+++ connect4.js
@@ -21,8 +21,28 @@ function setGame(){
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString(); // Assign a unique ID to each tile
             tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
             document.getElementById("board").append(tile);
         }
         board.push(row);
+    }
+}
+
+function setPiece(){
+    if(gameOver){
+        return;
+    }
+
+    let position = this.id.split("-");
+    let row = parseInt(position[0]);
+    let column = parseInt(position[1]);
+    board[row][column] = currentPlayer;
+    let tile = this;
+    if(currentPlayer == redPlayer){
+        tile.classList.add("redchip");
+        currentPlayer = yellowPlayer;
+    }else{
+        tile.classList.add("yellowchip");
+        currentPlayer = redPlayer;
     }
 }
