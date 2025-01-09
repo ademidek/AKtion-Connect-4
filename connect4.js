c63f4abb4739107bc3d66032becba64a5029c2a1
ademidek <ademidek@gmail.com>
2025-01-08 21:17:28 -0500

    updated game logic for wins

--- connect4.js
+++ connect4.js
@@ -56,4 +56,68 @@ function setPiece(){
 
     row = row - 1;
     currentColumn[column] = row;
+
+    endGame();
+}
+
+function endGame(){
+
+    //horizontal check
+    for(let r = 0; r < rows; r++){
+        //checking three spaces to the right to make sure we dont go out of bound
+        for(let c = 0; c < columns - 3; c++){
+            if(board[r][c] != " " && board[r][c] == board[r][c+1] && board[r][c] == board[r][c+2] && board[r][c] == board[r][c+3]){
+                setWinner(r,c);
+                return;
+            }
+        }       
+    }
+
+    //vertical check
+    for(let r = 0; r < rows - 3; r++){
+        //checking three spaces to the right to make sure we dont go out of bound
+        for(let c = 0; c < columns; c++){
+            if(board[r][c] != " " && board[r][c] == board[r+1][c] && board[r][c] == board[r+2][c] && board[r][c] == board[r+3][c]){
+                setWinner(r,c);
+                return;
+            }
+        }       
+    }
+
+    //diagonal checks
+    for(let r = 0; r < rows - 3; r++){
+        //checking three spaces to the right and the top to ensure we dont go out of bounds
+        for(let c = 0; c < columns - 3; c++){
+            if( board[r][c] != " " && 
+                board[r][c] == board[r + 1][c + 1] && 
+                board[r][c] == board[r + 2][c + 2] &&
+                board[r][c] == board[r + 3][c + 3]){
+                setWinner(r,c);
+                return;
+            }
+        }
+    }
+
+    for(let r = 3; r < rows; r++){
+        for(let c = 0; c < columns - 3; c++){
+            if(
+                board[r][c] != " " && 
+                board[r][c] == board[r - 1][c + 1] && 
+                board[r][c] == board[r - 2][c + 2] &&
+                board[r][c] == board[r - 3][c + 3]){
+                setWinner(r,c);
+                return;
+            }
+        }
+    }
+}
+
+function setWinner(r,c){
+    let winner = document.getElementById("winner");
+    if(board[r][c] == redPlayer){
+        winner.innerText = "Red Wins!";
+    }else{
+        winner.innerText = "Yellow Wins!";
+    }
+    gameOver = true;
 }
